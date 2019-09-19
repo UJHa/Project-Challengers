f7993b4a66368067b9d9663f1ee39b9b193eb277
UJHa <ddkk94@naver.com>
2019-09-19 13:36:58 +0900

    기사 캐릭터 이동 테스트
    기사 캐릭터 상하좌우 좌표 이동 구현

---
+++ Project-Challengers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Sprite[] sprites;
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start GameManager!!!");
+
+        sprites = Resources.LoadAll<Sprite>("0x72_16x16DungeonTileset.v4");
+        Debug.Log("sprites : "+sprites.Length);
+        //cube.transform.position = new Vector3(x, y, 0);
+        GameObject player = Instantiate(character);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
