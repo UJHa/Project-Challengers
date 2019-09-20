eb6f75819507453d3165790d7e23b101090226d2
UJHa <ddkk94@naver.com>
2019-09-20 13:27:17 +0900

    캐릭터 보간 이동 구현

--- Project-Challengers/Assets/Scripts/Knight.cs
+++ Project-Challengers/Assets/Scripts/Knight.cs
@@ -4,30 +4,62 @@ using UnityEngine;
 
 public class Knight : MonoBehaviour
 {
+    public float moveSpeed = 1.0f;
+    private Vector3 targetPosition;
+    private Vector3 moveTotalVector;
+    private bool isMoving;
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetPosition = transform.position;
+        isMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(!isMoving)
         {
-            transform.position = transform.position + new Vector3(0.0f, 1.0f, 0.0f);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                targetPosition = transform.position + new Vector3(0.0f, 1.0f, 0.0f);
+                isMoving = true;
+                moveTotalVector = targetPosition - transform.position;
+                //transform.position = transform.position + new Vector3(0.0f, 1.0f, 0.0f);
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                targetPosition = transform.position + new Vector3(0.0f, -1.0f, 0.0f);
+                isMoving = true;
+                moveTotalVector = targetPosition - transform.position;
+                //transform.position = transform.position + new Vector3(0.0f, -1.0f, 0.0f);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                targetPosition = transform.position + new Vector3(-1.0f, 0.0f, 0.0f);
+                isMoving = true;
+                moveTotalVector = targetPosition - transform.position;
+                //transform.position = transform.position + new Vector3(-1.0f, 0.0f, 0.0f);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                targetPosition = transform.position + new Vector3(1.0f, 0.0f, 0.0f);
+                isMoving = true;
+                moveTotalVector = targetPosition - transform.position;
+                //transform.position = transform.position + new Vector3(1.0f, 0.0f, 0.0f);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        if (isMoving)
         {
-            transform.position = transform.position + new Vector3(0.0f, -1.0f, 0.0f);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.position = transform.position + new Vector3(-1.0f, 0.0f, 0.0f);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position = transform.position + new Vector3(1.0f, 0.0f, 0.0f);
+             transform.position = transform.position + moveTotalVector * (moveSpeed * Time.deltaTime);
+            Debug.Log("transform.position : " + transform.position + "targetPosition : " + targetPosition);
+            Debug.Log(Vector3.Distance(transform.position, targetPosition));
+            if (Vector3.Distance(transform.position, targetPosition) < 0.02f)
+            {
+                transform.position = targetPosition;
+                isMoving = false;
+            }
         }
     }
 }
